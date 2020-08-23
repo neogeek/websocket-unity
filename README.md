4dca947ffc94bc66456e9fcdb9536e88452864ca
Scott Doxey <neogeek0101@gmail.com>
2020-08-23 00:01:52 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,20 @@
+# websocket-unity
+
+> Simple wrapper for the websocket-csharp library.
+
+## Installation
+
+### Unity Package Manager
+
+<https://docs.unity3d.com/Packages/com.unity.package-manager-ui@2.0/manual/index.html>
+
+#### Git
+
+```json
+{
+  "dependencies": {
+    "com.neogeek.websocket-unity": "https://github.com/neogeek/websocket-unity.git#v1.0.0",
+    ...
+  }
+}
+```
---
+++ Scripts/WebSocketController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+using WebSocketSharp;
+
+namespace WebSocketUnity
+{
+
+    public class WebSocketController : MonoBehaviour
+    {
+
+#pragma warning disable CS0649
+        [SerializeField]
+        private string _url;
+
+        [SerializeField]
+        private bool _logEventsInEditor;
+#pragma warning restore CS0649
+
+        public WebSocketEventHandler MessageHandler;
+
+        private readonly Queue<string> _messageQueue = new Queue<string>();
+
+        private WebSocket _webSocket;
+
+        public bool isConnected { get; private set; }
+
+        private void Awake()
+        {
+
+            _webSocket = new WebSocket(_url);
+
+            _webSocket.OnOpen += HandleOpen;
+            _webSocket.OnMessage += HandleMessage;
+            _webSocket.OnClose += HandleClose;
+            _webSocket.OnError += HandleError;
+
+        }
+
+        private void Update()
+        {
+
+            while (_messageQueue.Count > 0)
+            {
+
+                var message = _messageQueue.Dequeue();
+
+                if (_logEventsInEditor && Debug.isDebugBuild)
+                {
+
+                    Debug.Log($"Message received: {message}");
+
+                }
+
+                MessageHandler?.Invoke(message);
+
+            }
+
+        }
+
+        private void OnEnable()
+        {
+
+            _webSocket.Connect();
+
+        }
+
+        private void OnDisable()
+        {
+
+            _webSocket.Close();
+
+        }
+
+        private void HandleOpen(object sender, EventArgs e)
+        {
+
+            if (_logEventsInEditor && Debug.isDebugBuild)
+            {
+
+                Debug.Log($"Connected to {_url}");
+
+            }
+
+            isConnected = true;
+
+        }
+
+        private void HandleMessage(object sender, MessageEventArgs e)
+        {
+
+            _messageQueue.Enqueue(e.Data);
+
+        }
+
+        private void HandleClose(object sender, CloseEventArgs e)
+        {
+
+            if (_logEventsInEditor && Debug.isDebugBuild)
+            {
+
+                Debug.Log($"Disconnected from {_url}");
+
+            }
+
+            isConnected = false;
+
+        }
+
+        public static void HandleError(object sender, ErrorEventArgs e)
+        {
+
+            Debug.LogError(e.Message);
+
+        }
+
+        public void Send(string message)
+        {
+
+            _webSocket.Send(message);
+
+        }
+
+        public IEnumerator SendAsync(string message)
+        {
+
+            var completed = false;
+
+            _webSocket.SendAsync(message, _ => completed = true);
+
+            while (completed == false)
+            {
+
+                yield return null;
+
+            }
+
+        }
+
+        public async Task SendAwait(string message)
+        {
+
+            var completed = false;
+
+            _webSocket.SendAsync(message, _ => completed = true);
+
+            while (completed == false)
+            {
+
+                await Task.Yield();
+
+            }
+
+        }
+
+        [Serializable]
+        public class WebSocketEventHandler : UnityEvent<string>
+        {
+
+        }
+
+    }
+
+}
