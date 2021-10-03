bb72d715e8edb3fc7b64b09ff525550a3dd6a596
Scott Doxey <neogeek0101@gmail.com>
2021-10-03 14:52:47 -0400

    Whitespace.

--- Scripts/WebSocketController.cs
+++ Scripts/WebSocketController.cs
@@ -8,19 +8,14 @@ using WebSocketSharp;
 
 namespace WebSocketUnity
 {
-
     public class WebSocketController : MonoBehaviour
     {
-
 #pragma warning disable CS0649
-        [SerializeField]
-        protected string _url;
+        [SerializeField] protected string _url;
 
-        [SerializeField]
-        protected bool _logEventsInEditor;
+        [SerializeField] protected bool _logEventsInEditor;
 
-        [SerializeField]
-        protected float _keepAliveTimeout = 30;
+        [SerializeField] protected float _keepAliveTimeout = 30;
 #pragma warning restore CS0649
 
         public WebSocketEventHandler MessageHandler;
@@ -35,162 +30,117 @@ namespace WebSocketUnity
 
         protected void Awake()
         {
-
             _webSocket = new WebSocket(_url);
 
             _webSocket.OnOpen += HandleOpen;
             _webSocket.OnMessage += HandleMessage;
             _webSocket.OnClose += HandleClose;
             _webSocket.OnError += HandleError;
-
         }
 
         protected virtual void Update()
         {
-
             while (_messageQueue.Count > 0)
             {
-
                 var message = _messageQueue.Dequeue();
 
                 if (_logEventsInEditor && Debug.isDebugBuild)
                 {
-
                     Debug.Log($"Message received: {message}");
-
                 }
 
                 MessageHandler?.Invoke(message);
-
             }
 
             if (Time.time > _keepAliveNextTick)
             {
-
                 Send("ping");
 
                 _keepAliveNextTick = Time.time + _keepAliveTimeout;
-
             }
-
         }
 
         protected void OnEnable()
         {
-
             _webSocket.Connect();
-
         }
 
         protected void OnDisable()
         {
-
             _webSocket.Close();
-
         }
 
         protected void HandleOpen(object sender, EventArgs e)
         {
-
             if (_logEventsInEditor && Debug.isDebugBuild)
             {
-
                 Debug.Log($"Connected to {_url}");
-
             }
 
             isConnected = true;
-
         }
 
         protected virtual void HandleMessage(object sender, MessageEventArgs e)
         {
-
             _messageQueue.Enqueue(e.Data);
-
         }
 
         protected void HandleClose(object sender, CloseEventArgs e)
         {
-
             if (_logEventsInEditor && Debug.isDebugBuild)
             {
-
                 Debug.Log($"Disconnected from {_url}");
-
             }
 
             isConnected = false;
-
         }
 
         public static void HandleError(object sender, ErrorEventArgs e)
         {
-
             Debug.LogError(e.Message);
-
         }
 
         public void Send(string message)
         {
-
             if (isConnected && _webSocket.IsAlive)
             {
-
                 _webSocket.Send(message);
-
             }
-
         }
 
         public IEnumerator SendAsync(string message)
         {
-
             if (isConnected && _webSocket.IsAlive)
             {
-
                 var completed = false;
 
                 _webSocket.SendAsync(message, _ => completed = true);
 
                 while (completed == false)
                 {
-
                     yield return null;
-
                 }
-
             }
-
         }
 
         public async Task SendAwait(string message)
         {
-
             if (isConnected && _webSocket.IsAlive)
             {
-
                 var completed = false;
 
                 _webSocket.SendAsync(message, _ => completed = true);
 
                 while (completed == false)
                 {
-
                     await Task.Yield();
-
                 }
-
             }
-
         }
 
         [Serializable]
         public class WebSocketEventHandler : UnityEvent<string>
         {
-
         }
-
     }
-
 }
--- Scripts/WebSocketJsonController.cs
+++ Scripts/WebSocketJsonController.cs
@@ -10,87 +10,64 @@ using WebSocketSharp;
 
 namespace WebSocketUnity
 {
-
     public class WebSocketJsonController : WebSocketController
     {
-
         public WebSocketJsonEventHandler JsonMessageHandler;
 
         private static readonly JsonSerializerSettings JSON_SETTINGS =
-            new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore };
+            new JsonSerializerSettings {NullValueHandling = NullValueHandling.Ignore};
 
         private readonly Queue<JObject> _jsonMessageQueue = new Queue<JObject>();
 
         protected override void Update()
         {
-
             base.Update();
 
             while (_jsonMessageQueue.Count > 0)
             {
-
                 var message = _jsonMessageQueue.Dequeue();
 
                 if (_logEventsInEditor && Debug.isDebugBuild)
                 {
-
                     Debug.Log($"Message received: {JsonConvert.SerializeObject(message)}");
-
                 }
 
                 JsonMessageHandler?.Invoke(message);
-
             }
-
         }
 
         protected override void HandleMessage(object sender, MessageEventArgs e)
         {
-
             try
             {
-
                 var message = JObject.Parse(e.Data);
 
                 _jsonMessageQueue.Enqueue(message);
-
             }
             catch (Exception)
             {
-
                 _messageQueue.Enqueue(e.Data);
-
             }
-
         }
 
         public void Send(object message)
         {
-
             base.Send(JsonConvert.SerializeObject(message, Formatting.None, JSON_SETTINGS));
-
         }
 
         public IEnumerator SendAsync(object message)
         {
-
             return base.SendAsync(JsonConvert.SerializeObject(message, Formatting.None, JSON_SETTINGS));
-
         }
 
         public async Task SendAwait(object message)
         {
-
             await base.SendAwait(JsonConvert.SerializeObject(message, Formatting.None, JSON_SETTINGS));
-
         }
 
         [Serializable]
         public class WebSocketJsonEventHandler : UnityEvent<JObject>
         {
-
         }
-
     }
-
 }
